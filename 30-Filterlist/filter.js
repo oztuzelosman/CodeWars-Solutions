5d1e9c129d10a7b55c387c30a3049be09b77df5e
oztuzelosman <oztuzelosman@gmail.com>
2022-08-26 12:20:58 +0300

    solution of problem 30

--- 30-Filterlist/filter.js
+++ 30-Filterlist/filter.js
@@ -1,5 +1,6 @@
 function filter_list(l) {
-  return l.filter( num => num>=0)
+  return l.filter((num) =>{ if(typeof(num)==='number'){ return num>=0}})  
 }
+
 
 console.log(filter_list([1,2,'aasf','1','123',123]))
